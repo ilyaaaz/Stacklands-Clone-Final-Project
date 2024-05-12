bdfb28661f00e5d7e35710ddedd6bb3e6ba97360
Yoiimiiya <hz3735@nyu.edu>
2024-05-12 16:21:42 -0400

    most bugs fixed

--- Assets/Scripts/RestrictedArea.cs
+++ Assets/Scripts/RestrictedArea.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class RestrictedArea : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    Collider2D cld;
+
+    private void Start()
+    {
+        cld = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            cld.enabled = false;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            cld.enabled = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
         if (rb != null)
         {
-            rb.velocity = Vector2.zero; 
-            rb.AddForce(Vector2.down * 15f, ForceMode2D.Impulse); 
+            //rb.velocity = Vector2.zero; 
+            rb.AddForce(Vector2.down * 15f); 
         }
     }
 }
